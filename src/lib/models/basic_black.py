f1eff39534ff252b0487f4ca58216df51de33b94
KimKyungPyo <kyungpyo.kim0@gmail.com>
2026-02-27 19:01:13 +0900

    기본 모델 추가

---
+++ src/lib/models/basic_black.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, hidden_dim):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, hidden_dim, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(hidden_dim, out_channels, kernel_size=3, padding=1)
+
+        self.relu = nn.ReLU()
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        return x
