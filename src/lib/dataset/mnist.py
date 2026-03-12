4acebe2689a29daff5f28b6aa92239cea411a824
KimKyungPyo <kyungpyo.kim0@gmail.com>
2026-03-13 08:39:42 +0900

    mnist.py 추가

---
+++ src/lib/dataset/mnist.py
@@ -0,0 +1,8 @@
+from torchvision.datasets.mnist import MNIST
+from torchvision.transforms import ToTensor
+
+from lib.path import data_path
+
+
+def load_data(train=True):
+    return MNIST(data_path(), train=train, download=True, transform=ToTensor())
