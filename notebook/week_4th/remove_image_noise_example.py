bd3a8a8fad9e6a12b691faaacd8f578c030f3fa9
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2026-03-14 07:27:11 +0900

    학습 및 평가 코드 추가

--- notebook/week_4th/remove_image_noise_example.py
+++ notebook/week_4th/remove_image_noise_example.py
@@ -7,7 +7,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: study-club (3.11.14)
+#     display_name: dl-with-pytorch
 #     language: python
 #     name: python3
 # ---
@@ -118,3 +118,98 @@ class Encoder(nn.Module):
 
         return x
 
+
+
+# %%
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = BasicBlock(in_channels=8, out_channels=8, hidden_dim=8)
+        self.conv2 = BasicBlock(in_channels=8, out_channels=16, hidden_dim=16)
+
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=3, padding=1)
+
+        self.upsample1 = nn.ConvTranspose2d(8, 8, kernel_size=2, stride=2)
+        self.upsample2 = nn.ConvTranspose2d(16, 16, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.upsample1(x)
+        x = self.conv2(x)
+        x = self.upsample2(x)
+        x = self.conv3(x)
+
+        return x
+
+
+# %%
+class CAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.enc = Encoder()
+        self.dec = Decoder()
+
+    def forward(self, x):
+        x = self.enc(x)
+        x = self.dec(x)
+        x = torch.squeeze(x)
+
+        return x
+
+
+# %%
+import tqdm
+from torch.optim.adam import Adam
+from torch.utils.data.dataloader import DataLoader
+
+from lib.device import available_device
+from lib.path import model_path
+
+device = available_device()
+
+trainset = Denoising()
+train_loader = DataLoader(trainset, batch_size=32)
+
+model = CAE().to(device)
+
+lr = 1e-3
+optim = Adam(params=model.parameters(), lr=lr)
+
+criterion = nn.MSELoss()
+
+for ep in range(20):
+    iterator = tqdm.tqdm(train_loader)
+
+    for data, label in iterator:
+        optim.zero_grad()
+        pred = model(data.to(device))
+
+        loss = criterion(torch.sqeeze(pred), label.to(device))
+        loss.backward()
+        optim.step()
+        iterator.set_description(f'epoch{ep + 1} loss:{loss.item()}')
+
+torch.save(model.state_dict(), model_path('CAE.pth'))
+
+# %%
+model.cpu()
+
+with torch.no_grad():
+    model.load_state_dict(torch.load(model_path('CAE.pth'), map_location=device))
+
+    img = test_data.data[0]
+    gaussian = gaussian_noise(img)
+
+    input = torch.unsqueeze(gaussian, dim=0)
+    input.type(torch.FloatTensor)
+    input.to(device)
+    input = torch.unsqueeze(input, dim=0)
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(torch.squeeze(gaussian))
+    plt.subplot(1, 3, 2)
+    plt.imshow(torch.squeeze(gaussian))
+    plt.subplot(1, 3, 3)
+    plt.imshow(torch.squeeze(gaussian))
+    plt.show()
